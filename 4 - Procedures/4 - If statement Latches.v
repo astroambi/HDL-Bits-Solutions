417cb7e1d5a6f73d697748e7fab554893fdcf862
astroambi <147259027+astroambi@users.noreply.github.com>
2024-10-13 10:27:16 +0530

    Update 4 - If statement Latches.v
    because here we have to mention the else also which you not mention here that's why commit changes

--- 4 - Procedures/4 - If statement Latches.v
+++ 4 - Procedures/4 - If statement Latches.v
@@ -3,16 +3,24 @@ module top_module (
     output reg shut_off_computer,
     input      arrived,
     input      gas_tank_empty,
-    output reg keep_driving  ); //
+    output reg keep_driving
+);
 
+    // Always block for shut_off_computer control
     always @(*) begin
         if (cpu_overheated)
-           shut_off_computer = 1;
+            shut_off_computer = 1;
+        else
+            shut_off_computer = 0;  // Default value when CPU is not overheated
     end
 
+    // Always block for keep_driving control
     always @(*) begin
         if (~arrived)
-           keep_driving = ~gas_tank_empty;
+            keep_driving = ~gas_tank_empty;
+        else
+            keep_driving = 0;  // Default value when arrived
     end
 
 endmodule
+
